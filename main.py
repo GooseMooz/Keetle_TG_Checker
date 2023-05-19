67c967608bdcd401c7eb6f7bcdc61aeef11ad8b5
GooseMooz <goosev.goose@gmail.com>
2023-05-19 15:07:39 -0700

    wawa

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from cv2 import cv2
 import asyncio
 import telegram
+import os
+from dotenv import load_dotenv
 
-token = ""
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
 img = cv2.imread('dog.png', 0)
-rows, cols, _ = img.shape
+print(cv2.__version__)
+rows, cols = img.shape
+
+cv2.imshow('dog', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
 async def main():
-    bot = telegram.Bot(token)
-    print(cv2.__version__)
-
-    cv2.imshow('dog', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    bot = telegram.Bot(TOKEN)
     async with bot:
-        print(await bot.get_me())
+        await bot.send_message(text="wawa", chat_id=CHAT_ID)
+        await bot.send_document(chat_id=CHAT_ID, document='wawa.gif')
 
 
 if __name__ == '__main__':
