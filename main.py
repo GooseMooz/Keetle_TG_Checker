e2ea45be736427fc69bc68aac6d2326252e23d2c
GooseMooz <goosev.goose@gmail.com>
2023-05-25 18:10:26 -0700

    Version 1.0.0

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ TOKEN = os.getenv('TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 cam = cv2.VideoCapture(0)
 rectangle = [[0, 0], [0, 0]]
-avg_blue = [0.0, 0.0]
+avg_blue = [40.0, 40.0]
 work_state = [False, False]
+bang = [False]
 
 
 def fix_cords():
@@ -26,13 +27,13 @@ def fix_cords():
 
 
 def find_mean(area):
-    return np.average(area[:, :, 2])
+    return np.average(area[:, :, 0])
 
 
 def draw_rectangle(image, cords, color, cutout):
     orig = cutout[cords[0][1] + 10:cords[1][1] - 10, cords[0][0] + 10:cords[1][0] - 10]
     cv2.imshow('Cutout', orig)
-    print(orig[0, 0])
+    print(work_check(avg_blue, work_state, 40, find_mean(orig)))
     image[cords[0][1]:cords[1][1], cords[0][0]:cords[1][0]] = color
     image[cords[0][1] + 10:cords[1][1] - 10, cords[0][0] + 10:cords[1][0] - 10] = orig
 
@@ -47,49 +48,50 @@ def click_event(event, x, y, flags, params):
 
 def work_check(avg_arr, state_arr, avg_diff, new_avg):
     # FINISH THIS
+    print(new_avg)
+    print(avg_arr[1] - avg_arr[0], avg_diff)
+    print(state_arr)
     (avg_arr[0], avg_arr[1]) = (avg_arr[1], new_avg)
     if avg_arr[1] - avg_arr[0] > avg_diff:  # IF IT STARTS WORKING
         (state_arr[0], state_arr[1]) = (state_arr[1], True)
     elif avg_arr[1] - avg_arr[0] < -avg_diff:  # IF IT STOPS WORKING
         (state_arr[0], state_arr[1]) = (state_arr[1], False)
     else:
-        if state_arr[0]:
-            state_arr[1] = True
+        if state_arr[1]:
+            state_arr[0] = True
         else:
-            state_arr[1] = False
-    previous_state = state_arr[0]
-    current_state = state_arr[1]
-    if previous_state:
-        if not current_state:
-            return True
-        else:
-            return False
+            state_arr[0] = False
+
+    if state_arr == [True, False]:
+        bang[0] = True
+        return True
     else:
+        bang[0] = False
         return False
-
-
-while True:
-    ret, frame = cam.read()
-    temp = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    fix_cords()
-    cv2.imshow('Input', frame)
-    cv2.setMouseCallback('Input', click_event)
-    draw_rectangle(frame, rectangle, (255, 0, 0), temp)  # WHAT THE HELL
-
-    c = cv2.waitKey(1)
-    if c == 27:
-        break
-
-cam.release()
-cv2.destroyAllWindows()
 
 
 async def main():
     bot = telegram.Bot(TOKEN)
     async with bot:
-        if work_check(avg_blue, work_state, 30, find_mean(temp)):  # FIX THIS
-            await bot.send_document(chat_id=CHAT_ID, document='wawa.gif', caption="wawa")
+        await bot.send_message(chat_id=CHAT_ID, text="started working")
+        while True:
+            ret, frame = cam.read()
+            temp = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+            frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+            fix_cords()
+            draw_rectangle(frame, rectangle, (68, 54, 189), temp)
+            cv2.imshow('Input', frame)
+            cv2.setMouseCallback('Input', click_event)
+            if bang[0]:  # FIX THIS
+                await bot.send_document(chat_id=CHAT_ID, document='wawa.gif', caption="wawa")
+
+            c = cv2.waitKey(1)
+            if c == 27:
+                break
+        cam.release()
+        cv2.destroyAllWindows()
+
+        await bot.send_message(chat_id=CHAT_ID, text="bb")
 
 
 if __name__ == '__main__':
